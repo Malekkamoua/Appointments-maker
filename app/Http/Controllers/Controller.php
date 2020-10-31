0ec79e7c504ca3cc4d1f22927eb456e0e1e5f435
Malekkamoua <malekkamoua50@gmail.com>
2020-10-31 16:57:35 +0100

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -244,6 +244,8 @@ class Controller extends BaseController
                 ->orderBy('horaire', 'ASC')
                 ->first();
 
+                $date_dispo_init_cond = $date_dispo_init;
+
                 if($date_dispo_init == null ) {
 
                     $message = 'Aucune date n\'est disponible pour l\'instant. Veuillez appeler le 23 707 465 . ';
@@ -260,19 +262,48 @@ class Controller extends BaseController
                 // format actuel {"message":["2020","09","16"]}
                 $date_dispo_init = Carbon::createFromDate($pieces[0], $pieces[1], $pieces[2]);
 
+                $date_r = $date_dispo_init;
                 //Soustraire deux jours et verifier la disponibilité à nouveau
                 $date_dispo_init->toDateString();
                 
                 $date_to_string = $date_dispo_init->toDateString();
 
-                $date_dispo =  Horaire::where('date', '>=', $date_to_string)->where('horaire','>=',$t->format('H:i:s'))->orWhere('date', '>', $date_to_string)->where('Nature', '!=',  'V' )->where('gsm', null)->orderBy('date', 'ASC')
-                ->orderBy('horaire', 'ASC')
-                ->first();
+                $date = Carbon::parse($date_r);
+                $now = Carbon::now();
 
-                $pieces = explode("-", $date_dispo->date);
+                $diff = $date->diffInDays($now);
+
+
+                if($diff == 0) {
+
+                    $date_dispo =  Horaire::where(function($query) use($date_to_string,$t){
+                        $query->where('gsm', null)
+                        ->Where('date', $date_to_string)
+                        ->where('horaire','>=',$t->format('H:i:s'))
+                        ->where('gsm',null)
+                        ->where('Nature', '!=',  'V' )
+                        ->orderBy('date', 'ASC')
+                        ->orderBy('horaire', 'ASC');
+                    })
+                        ->orWhere('date', '>', $date_to_string)
+                        ->where('gsm',null)
+                        ->where('Nature', '!=',  'V' )
+                        ->orderBy('date', 'ASC')
+                        ->orderBy('horaire', 'ASC')
+                        ->first();
+
+                    }else {
+
+                        $date_dispo =  Horaire::where('date', '>=', $date_to_string)
+                        ->where('Nature', '!=',  'V' )
+                        ->where('gsm', null)
+                        ->orderBy('date', 'ASC')
+                        ->orderBy('horaire', 'ASC')
+                        ->first();
+
+                }
 
                 $date_dispo_rech = $date_dispo;
-
 
                 // format actuel {"message":["2020","09","16"]}
                 $date_dispo = Carbon::createFromDate($pieces[0], $pieces[1], $pieces[2]);
@@ -286,7 +317,6 @@ class Controller extends BaseController
                         'fiche' =>null
                     ]);                
                 }
-
 
                 if ($horaire != null) // this variable refer to horaire line 64 for update methode.
                 {
@@ -304,23 +334,39 @@ class Controller extends BaseController
                     ]); 
 
                 }else {
-                 
-                    $horaire_patient = Horaire::where(function($query) use($date_dispo_rech,$t){
-                        $query->where('gsm', null)
-                        ->Where('date', $date_dispo_rech->date)
-                        ->where('horaire','>=',$t->format('H:i:s'))
-                        ->where('gsm',null)
-                        ->where('Nature', '!=',  'V' )
-                        ->orderBy('date', 'ASC')
-                        ->orderBy('horaire', 'ASC');
-                    })
-                    ->orWhere('date', '>', $date_to_string)
-                    ->where('gsm',null)
-                    ->where('Nature', '!=',  'V' )
-                    ->orderBy('date', 'ASC')
-                    ->orderBy('horaire', 'ASC')
-                    ->first();
-                
+
+                    $date = Carbon::parse($date_r);
+                    $now = Carbon::now();
+    
+                    $diff = $date->diffInDays($now);
+
+                    if($diff == 0) {
+    
+                        $horaire_patient =  Horaire::where(function($query) use($date_dispo_init_cond,$t){
+                            $query->where('gsm', null)
+                            ->Where('date', $date_dispo_init_cond->date)
+                            ->where('horaire','>=',$t->format('H:i:s'))
+                            ->where('gsm',null)
+                            ->where('Nature', '!=',  'V' )
+                            ->orderBy('date', 'ASC')
+                            ->orderBy('horaire', 'ASC');
+                        })
+                            ->orWhere('date', '>', $date_dispo_init_cond->date)
+                            ->where('gsm',null)
+                            ->where('Nature', '!=',  'V' )
+                            ->orderBy('date', 'ASC')
+                            ->orderBy('horaire', 'ASC')
+                            ->first();
+
+                        }else {
+    
+                            $horaire_patient =  Horaire::where('date', '>=', $date_to_string)
+                            ->where('Nature', '!=',  'V' )
+                            ->where('gsm', null)
+                            ->orderBy('date', 'ASC')
+                            ->orderBy('horaire', 'ASC')
+                            ->first();
+                    }
 
                     if ($horaire_patient == null) {
                 
